b10bf06cf1f0f774e45bbabc65ee447bbce7b50e
gwoprk98 <gwoprk98@gmail.com>
2025-02-24 21:35:56 +0900

    feat: 주문 서비스

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -11,6 +13,7 @@ import java.util.List;
 @Entity
 @Table(name = "orders")
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id @GeneratedValue
@@ -33,7 +36,7 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
 
-    //==연관관꼐 메서드==//
+    //==연관관계 메서드==//
     public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -14,8 +14,8 @@ public class OrderRepository {
 
     private final EntityManager em;
 
-    public void save(OrderItem orderItem) {
-        em.persist(orderItem);
+    public void save(Order order) {
+        em.persist(order);
     }
 
     public Order findOne(Long id) {
