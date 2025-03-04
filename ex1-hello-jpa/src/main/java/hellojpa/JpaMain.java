2c62b19feb7ae94c11230a8fde1acc964185ccd2
gwoprk98 <gwoprk98@gmail.com>
2025-03-04 21:58:52 +0900

    feat: 준영속상태

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,15 +16,14 @@ public class JpaMain {
         tx.begin();
         try {
 
-            //비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("Jack");
-
             //영속
-            System.out.println("===BEFORE===");
-            em.persist(member);
-            System.out.println("===AFTER===");
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+
+            System.out.println("=====================");
 
             tx.commit();
         } catch (Exception e) {
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,13 +2,16 @@ package hellojpa;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
 public class Member {
+
+    public Member() {}
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     @Id
     private Long id;
